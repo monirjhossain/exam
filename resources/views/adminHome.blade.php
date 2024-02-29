ca32c484d52f957907bd38fb6c4d599d627c3daf
Md. Monir Hossain <59936758+monirjhossain@users.noreply.github.com>
2024-03-01 01:30:58 +0600

    Products

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -6,9 +6,20 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Admin Dashboard') }}</div>
-  
-                <div class="card-body">
-                    You are a Admin User.
+                <div class="container mx-auto">
+
+                    <div class="card-body text-center">
+                        <div class="card text-white bg-success mb-2 text-center" style="max-width: 20rem;">
+                        <div class="card-body">
+                            <a href="http://127.0.0.1:8000/categories " style="text-decoration:none;"><h5 class="card-title text-white">Categories</h5></a>
+                        </div>
+                        </div>
+                        <div class="card text-white bg-success mb-2" style="max-width: 20rem;">
+                        <div class="card-body">
+                            <a href="http://127.0.0.1:8000/products" style="text-decoration:none"><h5 class="card-title text-white">Products</h5></a>
+                        </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
